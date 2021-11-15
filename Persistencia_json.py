da90721401862858904d2ca5a023f430114b5cd9
AOCHOAG <aochoag@alumnos.nebrija.es>
2021-11-15 18:38:42 +0100

    Car_list Working 1 car type

--- Persistencia_json.py
+++ Persistencia_json.py
@@ -1,11 +1,10 @@
 import json
 def store(var, filename):
-    json.dump(json.dumps(var), open(filename, "w")) #Le estamos diiendo que almacene el valor de la variable en var
+    json.dump(json.dumps(var), open(filename, "a")) #Le estamos diiendo que almacene el valor de la variable en var
                                                     #Le decimos que lo escribamos en el archivo filename
 
 def retrieve(filename):
     var = json.loads(json.load(open(filename, "r")))
-    #print("\nMe has dicho:\n", var) # Imprimo la lista cargada (aparece llena)
     return var
 
 
--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import Persistencia_json as Pj #Importo persiste... y lo abrevio como Pj
+#algo = input("Dime algo: ")
+#nombre_archivo = input("Dime como quieres que se llame el archivo: ")
 
-algo = input("Dime algo: ")
-nombre_archivo = input("Dime como quieres que se llame el archivo: ")
-
-Pj.store(algo, nombre_archivo) #Llamo a la funcion store y le doy en orden que es lo que
+#Pj.store(algo, nombre_archivo) #Llamo a la funcion store y le doy en orden que es lo que
                                #Quiero que considere var y que es lo que quiero que considere
                                #filename
-
 #Pj.retrieve(nombre_archivo)
-print("\nMe has dicho:\n", Pj.retrieve(nombre_archivo)) # Imprimo la lista cargada (aparece llena)
+#print("\nMe has dicho:\n", Pj.retrieve(nombre_archivo)) # Imprimo la lista cargada (aparece llena)
+
+lista_coches = [] #Creamos una lista vacía
+
+datos_coche = {}  # Creo un diccionario vacio
+Nombre_archivo = input("Dime como quieres llamar al archivo: ")
+
+while True: #creamos un bucle, para introducir varios elementos
+    marca_coche = input("Introduce la marca del coche: ") #Creo variable marca coche y le pido que intruduzca
+    if marca_coche == "fin": #le digo que si se escribe "fin" finalice el bucle (con break)
+        break
+    modelo_coche = input("Introduce el modelo del coche: ")
+    combustible = input("introduce el tipo de combiustible: ")
+    cilindrada = input("Introduce la cilindrada: ")
+
+    datos_coche ["Marca del coche: "] = marca_coche #añado "palabras" al diccionario
+    datos_coche ["Modelo del coche: "] = modelo_coche
+    datos_coche ["Tipo de combustible: "] = combustible
+    datos_coche ["Cilindrada: "] = cilindrada
+
+    Pj.store(datos_coche, Nombre_archivo)
+
+print("Estos son los coches de la lista: ", Pj.retrieve(Nombre_archivo))
